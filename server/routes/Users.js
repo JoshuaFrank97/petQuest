d8f38e2f6d849184364316c539039bfd4975d13c
Joshua Frank <joshuafrank97@hotmail.com>
2020-09-04 16:01:50 -0500

    getuserbyemai and update user info routes completed

--- server/routes/Users.js
+++ server/routes/Users.js
@@ -3,6 +3,7 @@ const bcrypt = require("bcryptjs");
 const jwt = require("jsonwebtoken");
 const User = require("../model/User");
 const auth = require("../middleware/authentication");
+//const { add } = require("react-native-reanimated");
 
 
 router.post("/register", async(req, res) => {
@@ -146,23 +147,71 @@ router.post("/login", async (req, res) => {
 
 router.post("/update",auth,async (req,res) => {
     try{
-       const id = req.user;
-       console.log(id);
-       const {firstName, lastName ,email, password, address, phone} = req.user;
+        const id = req.user;
+    
+        const {firstName, lastName ,email, password, address, phone} = req.body;
 
-       const updatedUser = await  User.findOneAndUpdate({_id: id},{firstName: req.user.firstName},{useFindAndModify: false});
-       console.log(updatedUser);
+        if(password){
+            
+            if( password.length < 6 )
+                return res.status(400)
+                            .json({
+                                    msg: "Couldnt update your information : The password needs to be at least 6 charachters long."
+                                });
+            if(!password.match(/[A-Z]/g))
+                return res.status(400)
+                            .json({
+                                    msg: "Couldnt update your information : The password needs to have at least one uppercase letter."
+                                });
+            
+            if(!password.match(/[a-z]/g))
+                return res.status(400)
+                            .json({
+                                    msg: "Couldnt update your information :  The password needs to have at least one lower letter."
+                                });
+            
+            if(!password.match(/[0-9]/g))
+                return res.status(400)
+                            .json({
+                                    msg: "Couldnt update your information :  The password needs to have at least one number."
+                                });
+            
+            if(!password.match(/[*@!#%&()^~{}]/g))
+                return res.status(400)
+                            .json({
+                                    msg: "Couldnt update your information :  The password needs to have at least one special character."
+                                });
+
+            const salt = await bcrypt.genSalt();
+            const hash = await bcrypt.hash(password,salt);
+            req.body.password = hash; 
+        }
+
+        if(phone){
+
+            if(!phone.match(/^[0][1-9]\d{9}$|^[1-9]\d{9}$/g))
+                return res.status(400)
+                            .json({
+                                    msg: "Couldnt update your information :  Please use a valid phone number."
+                                });
+
+        }
+
+        const updatedUser = await  User.findOneAndUpdate({_id: id},req.body,{useFindAndModify: false});
+
+        const newinfo = await User.findById({_id: id});
         
-    }catch(err){
-        res.status(500)
-        .json({
-            error: err.message
-        });
+        res.json(newinfo);
+            
+        }catch(err){
+            res.status(500)
+            .json({
+                error: err.message
+            });
 
     }
 
 });
-
 
 // Delete a user header x-atuh-token  value token generated and saved on the front end when the user is logged in
 router.delete("/delete", auth, async (req, res) => {
@@ -175,6 +224,55 @@ router.delete("/delete", auth, async (req, res) => {
         error: err.message
     });
 }
+});
+
+//lookup for a user with email
+
+router.post("/getUserbyEmail",async(req,res) => {
+
+    const { email } = req.body;
+
+    const existingUser = await User.findOne({email : email})
+    if(!existingUser)
+        return res.status(400)
+                  .json({
+                      msg: " An account with that email doesnt exist"
+                  });
+    res.status(200).json({
+        firstName: existingUser.firstName,
+        lastName: existingUser.lastName,
+        email: email,
+        address: existingUser.address,
+        phone: existingUser.phone
+    });
+
+});
+//check if user is logged in or not
+router.post("/tokenIsValid", async(req, res) => {
+    try{
+        const token = req.header("x-auth-token");
+
+        if(!token) 
+            return res.json(false);
+        
+        const verified = jwt.verify(token, process.env.JWT_SECRET);
+        if(!verified)
+            return res.json(false);
+
+        const user = await User.findById(verified.id);
+
+        if(!user)
+            return res.json(false);
+
+        
+        return res.json(true);
+
+
+    }catch(err){
+        res.status(500).json({
+            err: err.message
+        });
+    }
 });
 
 
